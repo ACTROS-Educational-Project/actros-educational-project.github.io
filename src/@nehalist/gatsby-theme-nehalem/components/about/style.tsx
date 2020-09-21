6cd65e64941d130b1f11df9327131fe1fa0311d3
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-09-21 11:19:26 +0000

    [About]Page style modified

--- src/@nehalist/gatsby-theme-nehalem/components/about/style.tsx
+++ src/@nehalist/gatsby-theme-nehalem/components/about/style.tsx
@@ -4,8 +4,12 @@ import Theme from "../../styles/theme";
 
 export const Main = styled.article`
   text-align: center;
-  max-width: 100%;
+  max-width: 80%;
   position: relative;
+  margin-top: 0px;
+  margin-left: auto;
+  margin-right: auto;
+  background: #c2c0c0;
 `;
 
 export const Header = styled.h1`
@@ -19,12 +23,15 @@ export const MainDescription = styled.article`
   max-width: 60%;
   margin-left: auto;
   margin-right: auto;
+  margin-bottom: 0px;
+  padding: 0px;
 `;
 
 export const Grid = styled.article`
-  background: #fff;
   margin-bottom: 10px;
-  max-width: 80%;
+  margin-top: 0px;
+  padding: 0px;
+  max-width: 100%;
   margin-left: auto;
   margin-right: auto;
 `;
@@ -34,6 +41,8 @@ export const GridImg = styled.article`
   margin: 10px;
   display: inline-block;
   position: relative;
+  margin: 0px;
+  padding: 0px;
 `;
 
 export const GridImgImage = styled.article`
@@ -46,7 +55,7 @@ export const GridImgImage = styled.article`
 export const Map = styled.article`
 background: #fff;
 margin-bottom: 10px;
-max-width: 80%;
+max-width: 100%;
 margin-left: auto;
 margin-right: auto;
 `;
